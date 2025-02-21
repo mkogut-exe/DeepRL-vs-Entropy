80fc744d8a3bdbdb9fd6264a4feb0d201d8491e2
mkogut-exe <mkogut2003@gmail.com>
2025-02-21 14:45:04 +0100

    fixed roulette

--- Matching_roulette.py
+++ Matching_roulette.py
@@ -1,27 +1,26 @@
 from Wordle import Environment
 import random
+from tqdm import tqdm
 class Matching_roulette:#class that plays the game bu just choosing the word form the list of matching words at random
     def __init__(self, path):
         self.env = Environment(path)
 
 
-    def guess(self):
+    def play_one(self):
         while not self.env.end:
-            matches = self.env.get_matches()
+            matches = self.env.find_matches()
             len_matches = len(matches)
             if len_matches != 0:
                 self.env.guess(random.choice(self.env.allowed_words))
             else:
                 break
-            guess = self.env.get_guess()
-            self.env.guess(guess)
 
     def get_average(self, num_games):
         total_tries = 0
         wins=0
-        for _ in range(num_games):
+        for _ in tqdm(range(num_games)):
             self.env.reset()
-            self.guess()
+            self.play_one()
             total_tries += self.env.try_count
             if self.env.win:
                 wins += 1
