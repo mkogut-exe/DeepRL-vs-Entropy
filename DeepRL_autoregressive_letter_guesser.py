88027a917557b61a0ac25734728306c028b000f3
mkogut-exe <mkogut2003@gmail.com>
2025-03-31 11:52:22 +0200

    short run

--- DeepRL_autoregressive_letter_guesser.py
+++ DeepRL_autoregressive_letter_guesser.py
@@ -808,10 +808,10 @@ class Actor:
 
 
 env = Environment("reduced_set.txt")
-A = Actor(env, batch_size=5000, epsilon=0.1, learning_rate=1e-5, actor_repetition=10, critic_repetition=2,
-          random_batch=True, sample_size=1000)
+A = Actor(env, batch_size=1000, epsilon=0.1, learning_rate=1e-5, actor_repetition=10, critic_repetition=2,
+          random_batch=True, sample_size=250, display_progress_bar=True)
 # A.continue_training(model_path='GOOD2_actor_critic_end_Rv2_epo-40000_AR-10_CR-2_AS-8x256-Lr-1e-05-Bs-1024.pt', stats_path='GOOD2_actor_critic_stats_Rv2_epo-40000_AR-10_CR-2_AS-8x256-Lr-1e-05-Bs-1024.pkl', epochs=40000, print_freq=1000, learning_rate=1e-5, epsilon=0.1, actor_repetition=10, critic_repetition=2,batch_size=1024,random_batch=True,sample_size=256)
-A.train(epochs=500000, print_freq=5000, display_progress_bar=False)
+A.train(epochs=100000, print_freq=1000, display_progress_bar=True)
 
 
 
