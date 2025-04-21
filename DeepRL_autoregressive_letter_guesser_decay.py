0138a5ea30ee3d6edbde26ab5212e6b1ea344259
mkogut-exe <mkogut2003@gmail.com>
2025-04-21 21:30:07 +0200

    oops

--- DeepRL_autoregressive_letter_guesser_decay.py
+++ DeepRL_autoregressive_letter_guesser_decay.py
@@ -24,8 +24,9 @@ The training process involves generating words, calculating rewards based on let
 torch_version = torch.__version__
 cuda_available = torch.cuda.is_available()
 device = torch.device('cuda' if cuda_available else 'cpu')
-print(f'Torch version: {torch_version}, CUDA availability: {cuda_available}, Device: {device}')
-
+print(f'Torch version: {torch_version}, CUDA availability: {cuda_available}, Device: {device}, CUDA_VISIBLE_DEVICE:{os.environ.get("CUDA_VISIBLE_DEVICES")}')
+print("Current GPU:", torch.cuda.current_device())
+print("GPU Name:", torch.cuda.get_device_name(0))
 # Set random seed for reproducibility
 seed = 1
 torch.manual_seed(seed)
@@ -33,9 +34,9 @@ np.random.seed(seed)
 random.seed(seed)
 
 
-def create_model_id(epochs, actor_repetition, critic_repetition, actor_network_size, learning_rate, batch_size):
+def create_model_id(epochs, actor_repetition, critic_repetition, actor_network_size, learning_rate, batch_size,decay):
     timestamp = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
-    return f"_{timestamp}_ARLGv1-wd-win_epo-{epochs}_AR-{actor_repetition}_CR-{critic_repetition}_AS-{actor_network_size}-Lr-{learning_rate}-Bs-{batch_size}"
+    return f"_{timestamp}_ARLGv1-wd-win_epo-{epochs}_AR-{actor_repetition}_CR-{critic_repetition}_AS-{actor_network_size}-Lr-{learning_rate}-Bs-{batch_size}-Dec--{decay}"
     # - ARLGv1: Letter Guesser version 5
     # - +/-win: Model trained with(+)/without(-) win reward system
     # - epo: Number of training epochs
@@ -318,7 +319,8 @@ class Actor:
         # Generate unique model ID based on hyperparameters
         self.model_id = create_model_id(epochs=epochs, actor_repetition=self.actor_repetition,
                                         critic_repetition=self.critic_repetition, actor_network_size='1x256',
-                                        learning_rate=self.learning_rate, batch_size=self.batch_size)
+                                        learning_rate=self.learning_rate, batch_size=self.batch_size,
+                                        decay=self.weight_decay)
         # Initialize tracking variables
         total_wins = 0
         batch_losses_actor = []
@@ -830,11 +832,10 @@ class Actor:
 
 
 env = Environment("reduced_set.txt")
-A = Actor(env, batch_size=1, epsilon=0.1, learning_rate=1e-5, actor_repetition=1, critic_repetition=1,
-          random_batch=False, sample_size=1, display_progress_bar=False)
+A = Actor(env, batch_size=5000, epsilon=0.1, learning_rate=1e-5, actor_repetition=10, critic_repetition=2,
+          random_batch=False, sample_size=1000,weight_decay=5e-3, display_progress_bar=False)
 #A.continue_training(model_path='GOOD2_actor_critic_end_Rv2_epo-40000_AR-10_CR-2_AS-8x256-Lr-1e-05-Bs-1024.pt', stats_path='GOOD2_actor_critic_stats_Rv2_epo-40000_AR-10_CR-2_AS-8x256-Lr-1e-05-Bs-1024.pkl', epochs=500000, print_freq=5000,batch_size=5000,random_batch=True,sample_size=1000, learning_rate=1e-5, epsilon=0.1, actor_repetition=10, critic_repetition=2)
-A.train(epochs=5, print_freq=5, display_progress_bar=False)
-
+A.train(epochs=500000, print_freq=5000, display_progress_bar=False)
 
 
 
